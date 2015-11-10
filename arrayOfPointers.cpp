083e3e585f02b38ee449f4ebc01bf29a5d5ed07a
conor H <lackamoredude@gmail.com>
2015-11-10 22:37:23 +0000

    test file
    testing

---
+++ arrayOfPointers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+const int MAX = 3;
+
+int main()
+{
+	int var[MAX] = {10, 100, 200};
+	
+	for (int i = 0; i < MAX; i++)
+	{
+		cout << "Value of var[" << "] = ";
+		cout << var[i] <<endl;
+	}
+	return 0;
+}
