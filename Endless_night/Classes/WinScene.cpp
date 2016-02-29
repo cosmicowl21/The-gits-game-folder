8c8fd954ca746c7f84a1426fe3537b0b4ceeb634
cally.cagney@gmail.com <cally.cagney@gmail.com>
2016-02-29 22:04:24 +0000

    WinGame Explained

--- Endless_night/Classes/WinScene.cpp
+++ Endless_night/Classes/WinScene.cpp
@@ -1,3 +1,15 @@
+/*
+This is the Win game scene class
+When a player wins the game they will be directed to this scene
+They are congratulated and then given four options
+1. Try Again 
+2. LeaderBoard
+3. Quit
+4. Main Menu
+Once they choose an option they will be redirected to that screen
+*/
+
+
 #include "WinScene.h"
 #include "GameScene.h"
 #include "LeaderBoard.h"
@@ -36,50 +48,55 @@ bool WinScene::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();//init size
     Vec2 origin = Director::getInstance()->getVisibleOrigin();// init the origin 
 
+	//The Congratulations label is created, its position is set, the colour is set and the label is added as a child
 	auto label = Label::createWithTTF("CONGRADULATIONS", "fonts/Marker Felt.ttf", 44);
 	label->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height - label->getContentSize().height));
 	label->setColor(ccc3(255, 215, 0));
 	this->addChild(label, 1);
 
+	//The background sprite is created, its position is set and the sprite is added as a child
 	auto backgroundSprite = Sprite::create("win.png");// main menu image 
 	backgroundSprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	this->addChild(backgroundSprite); 
-
+	
 	auto titleSprite = Sprite::create();//image
 	titleSprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	this->addChild(titleSprite);
 
 	//button mapping to other screens
 
-	//when click play it goes to the game scene
-	//when click play it goes to the game scene
+	//When click play it goes to the game scene
 	auto NewGame = MenuItemImage::create("newGame1.png", "newGame1.png", CC_CALLBACK_1(WinScene::GoToGameScene, this));
 	NewGame->setPosition(Point(visibleSize.width / 4 + origin.x, visibleSize.height / 5 + origin.y));
 
-	//LeaderBoard
+	//When clicked it goes to the LeaderBoard scene
 	auto Leaderboard = MenuItemImage::create("leaderboard1.png", "leaderboard1.png", CC_CALLBACK_1(WinScene::GoToLeaderBoard, this));
 	Leaderboard->setPosition(Point(visibleSize.width / 2.5 + origin.x, visibleSize.height / 5 + origin.y));
 
-	//Exit button
+	//When clicked it Exits the game
 	auto closeItem = MenuItemImage::create("quit1.png", "quit1.png", CC_CALLBACK_1(WinScene::menuCloseCallback, this));
 	closeItem->setPosition(Point(visibleSize.width / 1.777 + origin.x, visibleSize.height / 5 + origin.y));
 
-	//Go to the main menu screen
+	//When clicked it goes to the main menu screen
 	auto MainMenu = MenuItemImage::create("Mainmenu.png", "Mainmenu.png", CC_CALLBACK_1(WinScene::GoToMainMenuScene, this));
 	MainMenu->setPosition(Point(visibleSize.width / 1.333 + origin.x, visibleSize.height / 5 + origin.y));
 	
+	//The new game menuItem is created, its position is set and this menuItem is added as a child
 	auto NewGameButton = Menu::create(NewGame, NULL);
 	NewGameButton->setPosition(Point::ZERO);
 	this->addChild(NewGameButton);
 
+	//The menuItem Menu is created, its position is set and this menuItem is added as a child
 	auto Menu = Menu::create(MainMenu, NULL);
 	Menu->setPosition(Point::ZERO);
 	this->addChild(Menu);
 	
+	//The menuItem Leaderboard is created, its position is set and this menuItem is added as a child
 	auto LeaderBoard = Menu::create(Leaderboard, NULL);
 	LeaderBoard->setPosition(Point::ZERO);
 	this->addChild(LeaderBoard);
 
+	//The menuItem ecxit is created, its position is set and this menuItem is added as a child
 	auto exit = Menu::create(closeItem, NULL);
 	exit->setPosition(Point::ZERO);
 	this->addChild(exit);
@@ -87,19 +104,19 @@ bool WinScene::init()
     return true;
 }
 
-void WinScene::GoToGameScene(Ref *sender)//go to game scene method implementation 
+void WinScene::GoToGameScene(Ref *sender)//This goes to game scene
 {
 	auto scene = GameScene::createScene();
 	Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
 }
 
-void WinScene::GoToMainMenuScene(Ref *sender)//go to game scene method implementation 
+void WinScene::GoToMainMenuScene(Ref *sender)//This goes to the main menu scene
 {
 	auto scene = MainMenuScene::createScene();
 	Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
 }
 
-void WinScene::GoToLeaderBoard(Ref *sender)//go to game scene method implementation 
+void WinScene::GoToLeaderBoard(Ref *sender)//This goes to the Leaderboard scene
 {
 	auto scene = LeaderBoard::createScene();
 	Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
